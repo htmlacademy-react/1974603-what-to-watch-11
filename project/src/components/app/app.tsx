0f38a67e6ec718c308350726008a0fb3a5d9ed00
Ml-diana <dbeskorovaynova777@gmail.com>
2022-10-27 10:02:24 +0300

    Добавление страниц

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,13 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+
+import MainPage from '../../pages/main-page';
+type AppProps = {
+  title: string;
+  genre: string;
+  releaseData: number;
+}
+
+function App({title, genre, releaseData} : AppProps): JSX.Element {
+  return <MainPage title={title} genre = {genre} releaseData = {releaseData}></MainPage>;
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const FilmData = {
+  TITLE: 'The Grand Budapest Hotel',
+  GENRE: 'Drama',
+  RELEASE_DATE: 2014
+}
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App title = {FilmData.TITLE} genre={FilmData.GENRE} releaseData={FilmData.RELEASE_DATE}/>
   </React.StrictMode>,
 );
