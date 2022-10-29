5e27208c778f863b1f979870c9712fda3d48b9bb
Ml-diana <dbeskorovaynova777@gmail.com>
2022-10-29 21:04:58 +0300

    checkkkkk

--- project/src/pages/film-page.tsx
+++ project/src/pages/film-page.tsx
@@ -33,7 +33,6 @@ function FilmPage(): JSX.Element {
               <span className="film-card__genre">Drama</span>
               <span className="film-card__year">2014</span>
             </p>
-
             <div className="film-card__buttons">
               <button className="btn btn--play film-card__button" type="button">
                 <svg viewBox="0 0 19 19" width="19" height="19">
@@ -53,13 +52,11 @@ function FilmPage(): JSX.Element {
           </div>
         </div>
       </div>
-
       <div className="film-card__wrap film-card__translate-top">
         <div className="film-card__info">
           <div className="film-card__poster film-card__poster--big">
             <img src="img/the-grand-budapest-hotel-poster.jpg" alt="The Grand Budapest Hotel poster" width="218" height="327" />
           </div>
-
           <div className="film-card__desc">
             <nav className="film-nav film-card__nav">
               <ul className="film-nav__list">
@@ -74,7 +71,6 @@ function FilmPage(): JSX.Element {
                 </li>
               </ul>
             </nav>
-
             <div className="film-rating">
               <div className="film-rating__score">8,9</div>
               <p className="film-rating__meta">
@@ -82,14 +78,10 @@ function FilmPage(): JSX.Element {
                 <span className="film-rating__count">240 ratings</span>
               </p>
             </div>
-
             <div className="film-card__text">
               <p>In the 1930s, the Grand Budapest Hotel is a popular European ski resort, presided over by concierge Gustave H. (Ralph Fiennes). Zero, a junior lobby boy, becomes Gustave's friend and protege.</p>
-
               <p>Gustave prides himself on providing first-class service to the hotel's guests, including satisfying the sexual needs of the many elderly women who stay there. When one of Gustave's lovers dies mysteriously, Gustave finds himself the recipient of a priceless painting and the chief suspect in her murder.</p>
-
               <p className="film-card__director"><strong>Director: Wes Anderson</strong></p>
-
               <p className="film-card__starring"><strong>Starring: Bill Murray, Edward Norton, Jude Law, Willem Dafoe and other</strong></p>
             </div>
           </div>
